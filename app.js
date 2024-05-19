9442f257e6c2f71ecfd1552d15c86f5a8fd46293
Franco Seiler <seilerfranco317@gmail.com>
2024-05-19 20:10:39 -0300

    sacala a baila

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 const express = require("express")
-const app = express()
 const path = require("path")
 const bcrypt = require('bcrypt')
-const redis = require('redis')
 const session = require('express-session')
-const RedisStore = require("connect-redis").default
+const redis = require('redis')
+const RedisStore = require("connect-redis")(session);
 require('dotenv').config();
+const { promisify } = require('util')
+const { formatDistance } = require("date-fns")
+
+const app = express()
+
 
 const redisHost = process.env.REDIS_HOST;
-const redisPort = process.env.REDIS_PORT;
+const redisPort = process.env.REDIS_PORT || 6379;
 const sessionSecret = process.env.SESSION_SECRET;
 
 // Conexión a Redis
-const redisClient = redis.createClient({
+const client = redis.createClient({
   socket: {
     host: redisHost,
     port: redisPort
   }
 })
+client.on('error', (err) => console.log('Redis Client Error', err));
 
-redisClient.connect().catch(console.error);
-redisClient.on('connect', () => {
-  console.log('Connected to Redis!');
-});
 
-// Initialize store.
-let redisStore = new RedisStore({
-  client: redisClient,
+
+const ahget = promisify(client.hget).bind(client)
+const asmembers = promisify(client.smembers).bind(client)
+const ahkeys = promisify(client.hkeys).bind(client)
+const aincr = promisify(client.incr).bind(client)
+const alrange = promisify(client.lrange).bind(client)
+
+
+// Middlewares para procesar datos JSON y datos codificados en URL.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// // Initialize store.
+const redisStore = new RedisStore({
+  client: client,
   prefix: "myapp:",
 })
 
@@ -36,150 +49,159 @@ app.use(
     store: redisStore,
     resave: false, // Mantiene la sesión activa
     saveUninitialized: false, // Solo guarda la sesión si hay datos
-    secret: "sessionSecret",
+    cookie: {
+      maxAge: 36000000,
+      httpOnly: false,
+      secure: false,
+    }, 
+    secret: sessionSecret,
   }),
 )
-
-// Middlewares para procesar datos JSON y datos codificados en URL.
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // Configuración del motor de plantillas para usar archivos HTML.
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 
 // Ruta GET para la página inicial.
-app.get("/", (req, res) => {
+app.get('/', async (req, res) => {
   if (req.session.userid) {
-    redisClient.hKeys("users", (err, users) => {
-      console.log(users)
-      res.render("dashboard", {
-        users,
+    const currentUserName = await ahget(`user:${req.session.userid}`, 'username')
+    const following = await asmembers(`following:${currentUserName}`)    
+    const users = await ahkeys('users')
+    
+    const timeline = []
+    const posts = await alrange(`timeline:${currentUserName}`, 0, 100)
+
+    for (post of posts) {
+      const timestamp = await ahget(`post:${post}`, 'timestamp')
+      const timeString = formatDistance(
+        new Date(),
+        new Date(parseInt(timestamp))
+      )
+
+      timeline.push({
+        message: await ahget(`post:${post}`, 'message'),
+        author: await ahget(`post:${post}`, 'username'),
+        timeString: timeString,
       })
+    }
+
+    res.render('dashboard', {
+      users: users.filter(
+        (user) => user !== currentUserName && following.indexOf(user) === -1
+      ),
+      currentUserName,
+      timeline
     })
   } else {
-    res.render("login")
+    res.render('login')
   }
 })
+
 
 // Ruta GET para la página de posteo de mensajes.
-app.get("/post", (req, res) => {
+app.get('/post', (req, res) => {
   if (req.session.userid) {
-    res.render("post")
+    res.render('post')
   } else {
-    res.render("login")
+    res.render('login')
   }
 })
 
+
 // Ruta POST para que antes de postear un mensaje, si no ingreso, lo redirija al login. Luego incrementa el valor del post id para que sea unico
-app.post("/post", async (req, res) => {
+app.post('/post', async (req, res) => {
   if (!req.session.userid) {
-    console.log("No session found, redirecting to login");
-    res.render("login");
-    return;
+    res.render('login')
+    return
+  }
+  
+  const { message } = req.body
+  const currentUserName = await ahget(`user:${req.session.userid}`, 'username')
+  const postid = await aincr('postid')
+  client.hmset(`post:${postid}`, 'userid', req.session.userid, 'username', currentUserName, 'message', message, 'timestamp', Date.now())
+  client.lpush(`timeline:${currentUserName}`, postid)
+
+  const followers = await asmembers(`followers:${currentUserName}`)
+  for (follower of followers) {
+    client.lpush(`timeline:${follower}`, postid)
   }
 
-  const { message } = req.body;
-  console.log("Message received:", message);
+  res.redirect('/')
+})
 
-  try {
-    const postid = await redisClient.incr("postid");
-    console.log("Post ID incremented to:", postid, "and the message is:", message);
-
-    await redisClient.hSet(
-      `post:${postid}`,
-      "userid", req.session.userid,
-      "message", message,
-      "timestamp", Date.now().toString()
-    );
-    
-    console.log("Post saved successfully with id:", postid);
-    res.render("dashboard");
-  } catch (err) {
-    console.error("Error interacting with Redis:", err);
-    res.render("error", { message: "Error creating post" });
+// Ruta POST para manejar los seguidores
+app.post('/follow', (req, res) => {
+  if (!req.session.userid) {
+    res.render('login')
+    return
   }
-});
 
+  const { username } = req.body
+  
+  client.hget(`user:${req.session.userid}`, 'username', (err, currentUserName) => {
+    client.sadd(`following:${currentUserName}`, username)
+    client.sadd(`followers:${username}`, currentUserName)
+  })
+
+  res.redirect('/')
+})
 
 // Ruta POST para manejar el login y el registro.
-app.post("/", async (req, res) => {
-  const { username, password } = req.body;
+app.post('/', (req, res) => {
+  const { username, password } = req.body
 
   if (!username || !password) {
-    return res.render("error", {
-      message: "Please set both username and password",
+    res.render('error', { 
+      message: 'Please set both username and password' 
+    })
+    return
+  }
+
+  const saveSessionAndRenderDashboard = userid => {
+    req.session.userid = userid
+    req.session.save()
+    res.redirect('/')
+  }
+
+  const handleSignup = (username, password) => {
+    client.incr('userid', async (err, userid) => {
+      client.hset('users', username, userid)
+
+      const saltRounds = 10
+      const hash = await bcrypt.hash(password, saltRounds)
+
+      client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+
+      saveSessionAndRenderDashboard(userid)
     })
   }
 
-  console.log(req.body);
-//  FALTARIA CAMBIAR ESTO PERO DE MOMENTO NO PUEDO ------------------
-  const saveSessionAndRenderDashboard = userid => {
-    // Guarda el ID del usuario en la sesión y renderiza el dashboard.
-    req.session.userid = userid;
-    req.session.save(err => {
-      if (err) {
-        return res.render('error', { message: 'Failed to save session' });
-      }
-      res.render("dashboard");
-    });
-  };
-
-
-  const handleSignup = async (username, password) => {
-    try {
-       // Crea un nuevo ID de usuario y lo asigna al nombre de usuario proporcionado.
-      const userid = await redisClient.incr('userid');
-      await redisClient.hSet('users', username, userid);
-
-      // Genera un hash de la contraseña y lo guarda junto con el nombre de usuario en Redis.
-      const saltRounds = 10;
-      const hash = await bcrypt.hash(password, saltRounds);
-      
-      await redisClient.hSet(`user:${userid}`, { hash, username });
-      
-      // Guarda la sesión y renderiza el dashboard.
-      saveSessionAndRenderDashboard(userid);
-    } catch (err) {
-      console.error(err);
-      res.render("error", { message: "Error during signup" });
-    }
-  }
-
-  const handleLogin = async (userid, password) => {
-    try {
-      // Obtiene el hash de la contraseña del usuario desde Redis
-      const hash = await redisClient.hGet(`user:${userid}`, 'hash');
-
-      // Compara la contraseña proporcionada con el hash almacenado.
-      const result = await bcrypt.compare(password, hash);
-
+  const handleLogin = (userid, password) => {
+    client.hget(`user:${userid}`, 'hash', async (err, hash) => {
+      const result = await bcrypt.compare(password, hash)
       if (result) {
-        saveSessionAndRenderDashboard(userid); // Si la contraseña es correcta, guarda la sesión y renderiza el dashboard.
+        saveSessionAndRenderDashboard(userid)
       } else {
-        res.render('error', { message: 'Incorrect password' }); // Si la contraseña es incorrecta, muestra un mensaje de error.
+        res.render('error', {
+          message: 'Incorrect password',
+        })
+        return
       }
-    } catch (err) {
-      console.error(err);
-      res.render("error", { message: "Error during login" });
-    }
+    })
   }
 
-  try {
-    // Busca el ID de usuario basado en el nombre de usuario proporcionado.
-    const userid = await redisClient.hGet('users', username);
-    if (!userid) {
-      // Si el usuario no existe, se realiza el registro
-      await handleSignup(username, password);
-    } else {
-      // Si el usuario existe, se realiza el inicio de sesión
-      await handleLogin(userid, password);
+  client.hget('users', username, (err, userid) => {
+    if (!userid) { //signup procedure
+      handleSignup(username, password)
+    } else { //login procedure
+      handleLogin(userid, password)
     }
-  } catch (err) {
-    console.error(err);
-    res.render("error", { message: "Error during authentication" });
-  }
+  })
 })
+
+
+
 
 app.listen(3000, () => console.log("Server ready"))
 
